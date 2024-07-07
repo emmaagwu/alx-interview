f74397ee886cf81aed372c7fa43522dce63ce271
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-07 18:44:13 +0100

    Refactor the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -18,5 +18,5 @@ def canUnlockAll(boxes):
         if boxIdx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)i
+    return n == len(seen_boxes)
 
